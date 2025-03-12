2d04b460fcf7ff39ced4bb39f3a68e806dfa4a66
Matias Tellini <tellini.matias@gmail.com>
2025-03-12 20:54:44 -0300

    border in input fixed

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -3,18 +3,18 @@ import { View, Text, TextInput } from "react-native";
 
 const InputField = (({ label, placeholder, value, onChangeText, ...textInputProps }: any) => {
   return (
-    <View className="gap-3">
-      <Text className="text-white uppercase text-lg font-semibold">
+    <View >
+      <Text className="mb-4 text-white uppercase text-lg font-semibold">
         {label}
       </Text>
       <TextInput
-
         placeholder={placeholder}
         placeholderTextColor="#ccc"
-        className={`p-5 rounded-lg border bg-gray-700 border-gray-600 text-white`}
+        className="p-4 rounded-lg border-1 text-left text-white border-red-500"
         value={value}
         onChangeText={onChangeText}
         {...textInputProps}
+        style={{ borderWidth: 1, borderColor: "#6b7280"}}
       />
     </View>
   );
